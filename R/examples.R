f97156d2e2e8204c5295dc4e08c185c67f230a3f
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2026-04-20 23:37:44 -0500

    Add 'examples' dataset and R documentation
    Add internal 'examples' dataset (data/examples.rda) and corresponding Roxygen documentation (R/examples.R). The dataset is a list of 11 objects intended for use in xsdm package examples.

---
+++ R/examples.R
@@ -0,0 +1,5 @@
+#' @title Examples 
+#' @description This is a list of examples objects for the xsdm package. 
+#' @format A list of 11 objects.
+#' @details Includes 11 objects to be used innerly in the xsdm examples 
+"examples"
